8b78cb590696b06b553423a3d68f3599d9b5373b
Dylan <dylan.lumsden@studytafensw.edu.au>
2023-06-12 17:52:40 +1000

    Changed Config.js

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyDb_hjZ2CztCoEF9gb-AFwNX_ux6Bk5-XE",
-    authDomain: "assessment-e12af.firebaseapp.com",
-    projectId: "assessment-e12af",
-    storageBucket: "assessment-e12af.appspot.com",
-    messagingSenderId: "629475444405",
-    appId: "1:629475444405:web:d4a22b0767733c2e6320fc"
+  apiKey: "AIzaSyAT9GQQlRsw9evni035E4aUpT21-u5WJP4",
+  authDomain: "assessment2-e4df2.firebaseapp.com",
+  projectId: "assessment2-e4df2",
+  storageBucket: "assessment2-e4df2.appspot.com",
+  messagingSenderId: "226419571419",
+  appId: "1:226419571419:web:588721ef71117a46c12afe"
   };
   
